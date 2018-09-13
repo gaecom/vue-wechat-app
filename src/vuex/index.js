691701332281038a7e43e6db9e4555ae4164d46e
kriswuwenxiang <794783332@qq.com>
2018-09-13 19:31:39 +0800

    添加表情数据状态管理

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -4,6 +4,7 @@ import state from './state'
 import getters from './getters'
 import mutations from './mutations'
 import actions from './actions'
+import chat from './modules/chat.js'
 
 Vue.use(Vuex)
 
@@ -13,6 +14,6 @@ export default new Vuex.Store({
   actions,
   getters,
   modules: {
-
+    chat,
   }
 })
